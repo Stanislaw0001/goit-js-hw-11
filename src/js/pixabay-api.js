eef151ba20a46b434eab6004352d630b4be224bc
VIKING <stas2017vasilenko@gmail.com>
2026-01-15 23:55:08 +0100

    hw-js

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const BASE_URL = "https://pixabay.com/api/";
+const API_KEY = "54179353-52b74677f97422d49d8297fd1";
+
+export async function getImagesByQuery(query) {
+  const response = await axios.get(BASE_URL, {
+    params: {
+      key: API_KEY,
+      q: query,
+      image_type: "photo",
+      orientation: "horizontal",
+      safesearch: true,
+    },
+  });
+  return response.data;
+}
